4e9f91e62a814b3951cb5d12d4017296835db92d
thegera4 <84020433+thegera4@users.noreply.github.com>
2022-10-24 20:58:55 -0500

    login con javascript y mui v5

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,14 @@
-import React from 'react';
-import Button from '@mui/material/Button';
-// import { BrowserRouter as Router, Switch, Route, Link } from "react-dom";
-// import logo from './logo.svg';
-// import './App.css';
-
-import Products from "./components/Products";
-
+import { Routes, Route } from 'react-router-dom';
+//import Dashboard from './Components/Dashboard/Dashboard';
+import MainRoutes from './MainRoutes';
 
 const App = () => {
   return (
-    <div className="App">
-      <Products />
+    <div>
+      <Routes >
+        <Route path="*" element={ <MainRoutes /> } />
+        {/* <Route exact path="/dashboard" element={ <Dashboard /> } /> */}
+      </Routes>  
     </div>
   )
 }
