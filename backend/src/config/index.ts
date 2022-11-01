0e889333f7a663552e366a4af01afd80b1923d96
Gonzalo Cocchetto <gonzalo.cocchetto@gmail.com>
2022-11-01 12:57:35 -0300

    Se realizaron cambios en la conexion con la base de datos

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -10,4 +10,10 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
 export default {
     port: process.env.PORT,
+    dataBase:{
+        db_name: process.env.DB_NAME,
+        db_user: process.env.DB_USER,
+        db_password: process.env.DB_PASSWORD
+    }
+
 }
--- backend/src/loaders/sequelize/index.ts
+++ backend/src/loaders/sequelize/index.ts
@@ -1,9 +1,15 @@
 import { Sequelize } from 'sequelize';
+import config from '../../config';
 
-const sequelize = new Sequelize('ecommerce-g11', 'root', 'root', {
+
+const dbName = <string>config.dataBase.db_name
+const dbUser = <string>config.dataBase.db_user
+const dbPassword = <string>config.dataBase.db_password
+
+const sequelize = new Sequelize(dbName, dbUser, dbPassword, {
 
     host: 'localhost',
-    dialect: 'mssql'
+    dialect: 'mysql'
   });
 
   
