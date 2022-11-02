e6a386016e262d0c78aca77ed284675465c3917c
thegera4 <thegera4@hotmail.com>
2022-11-01 21:27:51 -0600

    se agrega navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 //import Dashboard from './Components/Dashboard/Dashboard';
 import MainRoutes from './MainRoutes';
+import '../src/assets/fonts/arsenale_white.ttf'
+import '../src/assets/fonts/cambria.ttc'
 
 const App = () => {
   return (
