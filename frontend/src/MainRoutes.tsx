ecf4c53742e257311f8a03fd38840df1be1a0bfd
Leonardo Puebla <leonardo38361@gmail.com>
2022-11-04 14:55:40 -0300

    Ruta provisoria Products

--- frontend/src/MainRoutes.tsx
+++ frontend/src/MainRoutes.tsx
@@ -2,7 +2,7 @@ import './App.css';
 import React from 'react';
 import { Box } from '@mui/material';
 import { Routes, Route } from 'react-router-dom';
-//import Products from './components/Products';
+import {Products} from './components/containers/Products';
 import Login from './components/Login/Login';
 import Register from './components/Register/Register';
 import HomePage from './pages/Home/index';
@@ -13,8 +13,8 @@ function MainRoutes() {
     <Box>  
       {/* <Navbar /> */}
         <Routes >
-          {/* <Route path="/" element={ <Products /> } /> */}
-          <Route path="/" element={ <HomePage /> } />
+          <Route path="/" element={ <Products /> } />
+          {/* <Route path="/" element={ <HomePage /> } /> */}
           <Route path="/login" element={ <Login /> } />
           <Route path="/registrarse" element={ <Register /> } />
         </Routes>  
