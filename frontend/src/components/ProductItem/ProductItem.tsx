b7dc94d4d9d6ff6ce2a070fc9606b710448150e0
Leonardo Puebla <leonardo38361@gmail.com>
2022-11-07 23:08:11 -0300

    Se termina de listar cada atributo del mock

--- frontend/src/components/ProductItem/ProductItem.tsx
+++ frontend/src/components/ProductItem/ProductItem.tsx
@@ -1,17 +1,24 @@
-import React from "react";
-import ImageListItem from "@mui/material/ImageListItem";
-import ImageListItemBar from "@mui/material/ImageListItemBar";
-import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
-import  './ProductItem.css';
+import React from 'react';
+import ImageListItem from '@mui/material/ImageListItem';
+import ImageListItemBar from '@mui/material/ImageListItemBar';
+import Box from '@mui/material/Box';
+import './ProductItem.css';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import IconButton from '@mui/material/IconButton';
 
-const ProductItem = (data:any) => {
+const ProductItem = (data: any) => {
   return (
-    <Box sx={{ width: "200px", height: "300px", marginTop: "20px" }}>
-      <ImageListItem sx={{width: "200px", height: "200px"}} key={data.data.image}>
+    <Box sx={{ width: '200px', height: '300px', marginTop: '20px' }}>
+      <ImageListItem sx={{ width: '200px', height: '200px' }} key={data.data.image}>
         <img src={data.data.image} alt={data.data.title} loading="lazy" />
-        <Typography variant="body1" color="initial">{data.data.title}</Typography>
+        <ImageListItemBar
+          title={data.data.title}
+          subtitle={data.data.description}
+          actionIcon={
+            <IconButton>
+              <ShoppingCartOutlinedIcon sx={{ color: 'white' }} />
+            </IconButton>
+          }></ImageListItemBar>
       </ImageListItem>
     </Box>
   );
