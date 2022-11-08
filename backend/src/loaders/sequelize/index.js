c0d41d5499b9f8e4274a1e4c3f8a09afa096c699
Ulver <lorito.free@gmail.com>
2022-11-08 13:24:20 -0400

    Implementación de la automatización para la creación de la BD con sequelize, además de la creación de modelos users y usersdetails con sus respectivas relaciones

--- backend/src/loaders/sequelize/index.js
+++ backend/src/loaders/sequelize/index.js
@@ -1,12 +1,14 @@
-const { Sequelize } = require ('sequelize');
-const config = require ('../../config');
+const { Sequelize } = require('sequelize')
+const config = require('../../config')
 
+const sequelize = new Sequelize(
+  config.dataBase.name,
+  config.dataBase.username,
+  config.dataBase.password,
+  {
+    host: 'localhost',
+    dialect: 'mysql',
+  }
+)
 
-const sequelize = new Sequelize(config.dataBase.name, config.dataBase.username, config.dataBase.password, {
-
-  host: 'localhost',
-  dialect: 'mysql'
-
-});
-
-module.exports = sequelize;
+module.exports = sequelize
