57995c914c48dfac6943b87d7e3ef094e788e63a
Leonardo Puebla <leonardo38361@gmail.com>
2022-11-10 15:07:45 -0300

    se amplia el mock

--- frontend/src/mock/PinturasEcommerce.tsx
+++ frontend/src/mock/PinturasEcommerce.tsx
@@ -1,67 +1,91 @@
 const data = [
   {
     id: 1,
-    title: "Pintura1",
+    title: 'Pintura1',
     image:
-      "https://images.pexels.com/photos/1269968/pexels-photo-1269968.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    description: "Hermosa pintura abstracta para decorar tu hogar",
-    price: 1200,
+      'https://images.pexels.com/photos/1269968/pexels-photo-1269968.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    description: 'Hermosa pintura abstracta para decorar tu hogar',
+    price: 1200
   },
   {
     id: 2,
-    title: "Pintura2",
+    title: 'Pintura2',
     image:
-      "https://images.pexels.com/photos/1585325/pexels-photo-1585325.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    description: "Hermosa pintura abstracta para decorar tu hogar",
-    price: 1200,
+      'https://images.pexels.com/photos/1585325/pexels-photo-1585325.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    description: 'Hermosa pintura abstracta para decorar tu hogar',
+    price: 1200
+  },
+  {
+    id: 11,
+    title: 'Pintura11',
+    image:
+      'https://images.pexels.com/photos/1585325/pexels-photo-1585325.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    description: 'Hermosa pintura abstracta para decorar tu hogar',
+    price: 1200
   },
   {
     id: 3,
-    title: "Pintura3",
+    title: 'Pintura3',
     image:
-      "https://images.pexels.com/photos/1572386/pexels-photo-1572386.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    description: "Hermosa pintura abstracta para decorar tu hogar",
-    price: 1200,
+      'https://images.pexels.com/photos/1572386/pexels-photo-1572386.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    description: 'Hermosa pintura abstracta para decorar tu hogar',
+    price: 1200
   },
   {
     id: 4,
-    title: "Pintura4",
+    title: 'Pintura4',
     image:
-      "https://images.pexels.com/photos/1572386/pexels-photo-1572386.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    description: "Hermosa pintura abstracta para decorar tu hogar",
-    price: 1200,
+      'https://images.pexels.com/photos/1572386/pexels-photo-1572386.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    description: 'Hermosa pintura abstracta para decorar tu hogar',
+    price: 1200
   },
   {
     id: 5,
-    title: "Pintura5",
+    title: 'Pintura5',
     image:
-      "https://images.pexels.com/photos/1266808/pexels-photo-1266808.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    description: "Hermosa pintura abstracta para decorar tu hogar",
-    price: 1200,
+      'https://images.pexels.com/photos/1266808/pexels-photo-1266808.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    description: 'Hermosa pintura abstracta para decorar tu hogar',
+    price: 1200
   },
   {
     id: 6,
-    title: "Pintura6",
+    title: 'Pintura6',
     image:
-      "https://images.pexels.com/photos/1234853/pexels-photo-1234853.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    description: "Hermosa pintura abstracta para decorar tu hogar",
-    price: 1200,
+      'https://images.pexels.com/photos/1266808/pexels-photo-1266808.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    description: 'Hermosa pintura abstracta para decorar tu hogar',
+    price: 1200
   },
   {
     id: 7,
-    title: "Pintura7",
+    title: 'Pintura7',
     image:
-      "https://images.pexels.com/photos/1234853/pexels-photo-1234853.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    description: "Hermosa pintura abstracta para decorar tu hogar",
-    price: 1200,
+      'https://images.pexels.com/photos/1266808/pexels-photo-1266808.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    description: 'Hermosa pintura abstracta para decorar tu hogar',
+    price: 1200
   },
   {
     id: 8,
-    title: "Pintura8",
+    title: 'Pintura8',
     image:
-      "https://images.pexels.com/photos/1234853/pexels-photo-1234853.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    description: "Hermosa pintura abstracta para decorar tu hogar",
-    price: 1200,
+      'https://images.pexels.com/photos/1234853/pexels-photo-1234853.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    description: 'Hermosa pintura abstracta para decorar tu hogar',
+    price: 1200
   },
+  {
+    id: 9,
+    title: 'Pintura9',
+    image:
+      'https://images.pexels.com/photos/1234853/pexels-photo-1234853.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    description: 'Hermosa pintura abstracta para decorar tu hogar',
+    price: 1200
+  },
+  {
+    id: 10,
+    title: 'Pintura10',
+    image:
+      'https://images.pexels.com/photos/1234853/pexels-photo-1234853.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    description: 'Hermosa pintura abstracta para decorar tu hogar',
+    price: 1200
+  }
 ];
 export default data;
