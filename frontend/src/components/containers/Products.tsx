6e2438e1e8cb80209460cbaa0b69fdd441c8f8b2
Leonardo Puebla <leonardo38361@gmail.com>
2022-11-10 15:07:17 -0300

    cambios en componente

--- frontend/src/components/containers/Products.tsx
+++ frontend/src/components/containers/Products.tsx
@@ -1,24 +1,37 @@
-import React from "react";
-import ProductItem from "../ProductItem/ProductItem";
-import { Container } from "@mui/system";
-import Grid from "@mui/material/Grid";
-import useMock from "../hooks/useMock";
-import { ProductsI } from "../interfaces/ProductsI";
+import React from 'react';
+import MainProductItem from '../ProductItem/MainProductItem/MainProductItem';
+import { Container } from '@mui/system';
+import Grid from '@mui/material/Grid';
+import useMock from '../hooks/useMock';
+import ProductsItemOutstanding from '../ProductItem/ProductsItemOutstanding/ProductItemOutstanding';
+import OtherProducts from '../ProductItem/OtherProducts/OtherProducts';
 
 export const Products = () => {
   const data = useMock();
   return (
     <Container>
-      <Grid
-        container
-        spacing={{ xs: 2, md: 3 }}
-        columns={{ xs: 4, sm: 8, md: 20 }}
-      >
-        {data.map((data) => (
-          <Grid item xs={5}>
-            <ProductItem key={data.id} data={data} />
-          </Grid>
-        ))}
+      <Grid container rowSpacing={2} columnSpacing={{ xs: 1, sm: 3, md: 3, lg: 5 }} justifyContent="flex-start">
+        {data?.map((data, i) =>
+          i === 0 ? (
+            <React.Fragment key={data.id}>
+              <Grid item xs={12} sm={12} md={12}>
+                <MainProductItem data={data} />
+              </Grid>
+            </React.Fragment>
+          ) : i > 0 && i <= 2 ? (
+            <React.Fragment key={data.id}>
+              <Grid item xs={6} sm={6} md={6}>
+                <ProductsItemOutstanding data={data} />
+              </Grid>
+            </React.Fragment>
+          ) : (
+            <React.Fragment key={data.id}>
+              <Grid item xs={6} sm={3} md={3}>
+                <OtherProducts data={data} />
+              </Grid>
+            </React.Fragment>
+          )
+        )}
       </Grid>
     </Container>
   );
