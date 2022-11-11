395ce1f1ce67c097d9d92e02a2e7b1e4af36ee1a
Leonardo Puebla <leonardo38361@gmail.com>
2022-11-11 20:38:52 -0300

    cambio de directorio

--- frontend/src/components/hooks/useMock.tsx
+++
@@ -1,9 +0,0 @@
-import { useState } from "react";
-import data from "../../mock/PinturasEcommerce";
-import { ProductsI } from "../interfaces/ProductsI";
-
-const useMock = () => {
-  const [mockPinturas, setMockPinturas] = useState(data);
-  return mockPinturas;
-};
-export default useMock;
