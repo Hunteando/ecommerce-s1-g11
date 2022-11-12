2b1dcdb730df87f41a0d7225083f8fee66078b88
Ulver <lorito.free@gmail.com>
2022-11-12 15:13:55 -0400

    Creación de los modelos de usuario con sequelize además de la Base de Datos

--- backend/src/loaders/sequelize/index.js
+++ backend/src/loaders/sequelize/index.js
@@ -1,5 +1,5 @@
 const { Sequelize } = require('sequelize')
-const config = require('../../config')
+const config = require('../../config/environment')
 
 const sequelize = new Sequelize(
   config.dataBase.name,
--- backend/src/loaders/server/expressServer.js
+++ backend/src/loaders/server/expressServer.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const config = require('../../config')
+const config = require('../../config/environment')
 const sequelize = require('../sequelize')
 const mysql = require('mysql2/promise')
 require('../../libs/relations')
