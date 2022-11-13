0cfdd9433758e9e4f89300b9d154b23d03f296e8
Leonardo Puebla <leonardo38361@gmail.com>
2022-11-13 02:31:44 -0300

    se paracion de configuracion webpack production y webpack development + nueva regla ts

--- frontend/webpack.config.dev.js
+++ frontend/webpack.config.dev.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
@@ -14,21 +13,20 @@ module.exports = {
         assetModuleFilename: 'assets/images/[hash][ext][query]',
         publicPath: '/'
     },
+    mode: 'development',
     resolve: {
-        extensions: ['.js', '.tsx', '.ts', '.jsx'],
+        extensions: ['.js', '.tsx', 'ts'],
         alias: {
             '@component': path.resolve(__dirname, 'src/components/'),
             '@context': path.resolve(__dirname, 'src/context/'),
             '@hooks': path.resolve(__dirname, 'src/hooks/'),
             '@containers': path.resolve(__dirname, 'src/containers/'),
             '@pages': path.resolve(__dirname, 'src/pages/'),
-            '@icons': path.resolve(__dirname, 'src/assets/icons/'),
-            '@logos': path.resolve(__dirname, 'src/assets/logos/'),
-            '@styles': path.resolve(__dirname, 'src/styles/'),
             '@utils': path.resolve(__dirname, 'src/utils/')
         }
     },
-    mode: 'production',
+    watch: true,
+    mode: 'development',
     module: {
         rules: [
             {
@@ -72,7 +70,9 @@ module.exports = {
                 use: [
                     {
                         loader: 'svg-url-loader',
-                        options: {},
+                        options: {
+                            limit: 10000,
+                        },
                     },
                 ],
             },
@@ -94,21 +94,13 @@ module.exports = {
         new MiniCssExtractPlugin({
             filename: 'assets/[name].[contenthash].css'
         }),
-        new CleanWebpackPlugin(),
-        new CopyPlugin({
-            patterns: [
-                {
-                    from: path.resolve(__dirname, "src", "assets/images"),
-                    to: "assets/images"
-                }
-            ]
-        }),
     ],
-    optimization: {
-        minimize: true,
-        minimizer: [
-            new CssMinimizerPlugin(),
-            new TerserPlugin(),
-        ]
-    },
+    devServer: {
+        historyApiFallback: true,
+        static: {
+            directory: path.join(__dirname, 'public'),
+        },
+        compress: true,
+        port: 64340
+    }
 }
