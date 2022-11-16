5b9eb44fa61650e6a73cfae1bc73fe141257f213
Leonardo Puebla <leonardo38361@gmail.com>
2022-11-15 22:43:07 -0300

    se agrega un carousel con productos

--- frontend/src/containers/Products.tsx
+++ frontend/src/containers/Products.tsx
@@ -1,39 +1,53 @@
 import * as React from 'react';
-import MainProductItem from '../components/ProductItem/MainProductItem/MainProductItem';
 import { Container } from '@mui/system';
 import Grid from '@mui/material/Grid';
-import useMock from '../hooks/useMock';
-import ProductsItemOutstanding from '../components/ProductItem/ProductsItemOutstanding/ProductItemOutstanding';
-import OtherProducts from '../components/ProductItem/OtherProducts/OtherProducts';
-import { ProductsI } from '../interfaces/ProductsI';
+import ProductsItemOutstanding from '../components/ProductItem/ProductItemOutstanding';
+import FirstFeaturedProduct from '../components/ProductItem/FirstFeaturedProduct';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import { Box, Typography } from '@mui/material';
+import useMock from './../hooks/useMock';
+import { ProductsI } from 'src/interfaces/ProductsI';
+import './Products.css';
+import { settings } from '../utils/carousel-settings';
+import Paper from '@mui/material/Paper';
 
-export const Products = () => {
+export const Products = (): any => {
   const data: ProductsI[] = useMock();
+
   return (
-    <Container>
-      <Grid container rowSpacing={2} columnSpacing={{ xs: 1, sm: 3, md: 3, lg: 5 }} justifyContent="flex-start">
-        {data?.map((data: { id: React.Key; }, i: number) =>
-          i === 0 ? (
-            <React.Fragment key={data.id}>
-              <Grid item xs={12} sm={12} md={12}>
-                <MainProductItem data={data} />
-              </Grid>
-            </React.Fragment>
-          ) : i > 0 && i <= 2 ? (
-            <React.Fragment key={data.id}>
-              <Grid item xs={6} sm={6} md={6}>
-                <ProductsItemOutstanding data={data} />
-              </Grid>
-            </React.Fragment>
-          ) : (
-            <React.Fragment key={data.id}>
-              <Grid item xs={6} sm={3} md={3}>
-                <OtherProducts data={data} />
-              </Grid>
-            </React.Fragment>
-          )
-        )}
-      </Grid>
-    </Container>
+    <>
+      <Container sx={{ width: '100%' }}>
+        <Grid container rowSpacing={2} columnSpacing={{ xs: 1, sm: 3, md: 3, lg: 5 }} justifyContent="flex-start">
+          <FirstFeaturedProduct />
+        </Grid>
+        <Grid container columnSpacing={{ xs: 1, sm: 3, md: 3, lg: 5 }}>
+          <ProductsItemOutstanding />
+        </Grid>
+      </Container>
+      <Container>
+        <Typography variant="h2" color="initial">
+          Title
+        </Typography>
+        <Slider {...settings}>
+          {data.map((data, i) => (
+            <div key={data.id} className="container">
+              <Box>
+                <Paper>
+                  <img src={data.image} alt="" style={{ height: '250px', width: '100%' }} />
+                </Paper>
+                <Typography variant="h5" color="initial">
+                  {data.title}
+                </Typography>
+              </Box>
+              <Box>
+                <Typography variant="h5" color="initial">{`${data.price}`}</Typography>
+              </Box>
+            </div>
+          ))}
+        </Slider>
+      </Container>
+    </>
   );
 };
