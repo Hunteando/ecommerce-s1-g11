7312754d50b6db98619582e32ef05fa2ee55ced5
martinsetaro <lordsesony@gmail.com>
2022-11-18 19:39:44 -0300

    arreglos vista productos

--- frontend/src/containers/Products.tsx
+++ frontend/src/containers/Products.tsx
@@ -3,6 +3,9 @@ import { Box } from '@mui/system'
 import { Button, Typography } from '@mui/material'
 import  PinturasEcommerce  from '../mock/PinturasEcommerce';
 import { Link } from 'react-router-dom'
+import './productos.css'
+import ArrowForwardIosOutlinedIcon from '@mui/icons-material/ArrowForwardIosOutlined';
+import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
 
 
 
@@ -20,22 +23,22 @@ const Products = () => {
         Listado de Productos
       </Typography>
 
-      <Box component="div" sx={{ width:'100%' , height:'auto', display:'flex' , pb:2, marginTop:'4rem' ,justifyContent:'space-around', flexWrap:'wrap'} }>
+      <Box component="div" sx={{ width:'100%' , height:'auto', display:'flex' , pb:2, marginTop:'4rem' ,justifyContent:'space-around', flexWrap:'wrap', marginBottom:'3rem'} }>
    
       {productos.map(item => {
         return(
-          <Box key={item.id} sx={{ width:'15rem' , height:'30rem'} } >
-            <Box component="img" src={item.image} alt="img" sx={{width:'100%', height:'10rem'}}/>
-            <Typography component="h2" variant="h2" sx={{fontSize:'2rem', fontFamily:'comspotM'}}>
-              {item.title}
+          <Box key={item.id} sx={{ width:'23rem' , height:'30rem', marginBottom:'4rem'} } className="boxHover" >
+            <Box component="img" src={item.image} alt="img" sx={{width:'100%', height:'15rem'}}/>
+            <Typography component="h2" variant="h2" sx={{fontSize:'2rem', fontFamily:'comspotM', marginLeft:'2rem'}}>
+              <FavoriteBorderOutlinedIcon sx={{marginRight:'2rem'}}/>{item.title}
             </Typography>
-            <Typography component="h4" variant="h4" sx={{fontSize:'1.5rem', fontFamily:'comspotExI'}} >
+            <Typography component="h4" variant="h4" sx={{fontSize:'1.5rem', fontFamily:'comspotExI', marginLeft:'2rem'}} >
               {item.description}
             </Typography>
-            <Typography component="h3" variant="h3" sx={{fontSize:'2.5rem', fontFamily:'comspotM'}} >
-             $ {item.price}
+            <Typography component="h3" variant="h3" sx={{fontSize:'2.5rem', fontFamily:'comspotM', marginLeft:'2rem'}} >
+             Precio:$ {item.price}
             </Typography>
-            <Link to={`/DetalleProducto/elementos/${item.id}`} ><Button>Más detalle</Button></Link>
+            <Link to={`/DetalleProducto/elementos/${item.id}`} ><Button sx={{marginTop:'3rem'}}>Más detalle <ArrowForwardIosOutlinedIcon/> </Button></Link>
           </Box>
           
         )
@@ -45,5 +48,7 @@ const Products = () => {
     </Box>
   )
 }
+
+
 
 export default Products
