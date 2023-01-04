0eced19f4a5936d25f45c4c24f6d5922f522eda9
JC <juancruzmusso@gmail.com>
2023-01-04 20:52:05 +0100

    comentado relaciones por errores

--- backend/src/libs/relations.js
+++ backend/src/libs/relations.js
@@ -1,14 +1,14 @@
 const Addresses = require('../models/addresses')
 const Cart = require('../models/cart')
 const Categorys = require('../models/categorys')
-const Image = require('../models/images')
 const OrderSummary = require('../models/orderSummary')
-const Product = require('../models/products')
 const Reviews = require('../models/reviews')
 const TypeDocument = require('../models/typedocument')
 
 const User = require('../models/users')
 const UserDetails = require('../models/usersdetails')
+const Product = require('../models/products')
+const Image = require('../models/images')
 
 User.hasOne(UserDetails, {
   onDelete: 'CASCADE',
@@ -25,61 +25,61 @@ UserDetails.belongsTo(User, {
 //   onDelete: 'CASCADE',
 // })
 
-UserDetails.hasMany(Addresses, {
-  onDelete: 'CASCADE',
-})
-Addresses.belongsTo(UserDetails, {
-  onDelete: 'CASCADE',
-})
+// UserDetails.hasMany(Addresses, {
+//   onDelete: 'CASCADE',
+// })
+// Addresses.belongsTo(UserDetails, {
+//   onDelete: 'CASCADE',
+// })
 
-/* çççççççççççççççççççç */
+// /* çççççççççççççççççççç */
 
-UserDetails.hasOne(TypeDocument, {
-  onDelete: 'CASCADE',
-})
-TypeDocument.belongsTo(UserDetails, {
-  onDelete: 'CASCADE',
-})
+// UserDetails.hasOne(TypeDocument, {
+//   onDelete: 'CASCADE',
+// })
+// TypeDocument.belongsTo(UserDetails, {
+//   onDelete: 'CASCADE',
+// })
 
-/////////////////////////////////////////////////////////////
+// /////////////////////////////////////////////////////////////
 
-UserDetails.hasMany(Addresses, { onDelete: 'CASCADE' })
-Addresses.belongsTo(UserDetails, { onDelete: 'CASCADE' })
+// UserDetails.hasMany(Addresses, { onDelete: 'CASCADE' })
+// Addresses.belongsTo(UserDetails, { onDelete: 'CASCADE' })
 
-/////////////////////////////////////////////////////////////
+// /////////////////////////////////////////////////////////////
 
-UserDetails.hasOne(Reviews, { onDelete: 'CASCADE' })
-Reviews.belongsTo(UserDetails, { onDelete: 'CASCADE' })
+// UserDetails.hasOne(Reviews, { onDelete: 'CASCADE' })
+// Reviews.belongsTo(UserDetails, { onDelete: 'CASCADE' })
 
-/////////////////////////////////////////////////////////////
+// /////////////////////////////////////////////////////////////
 
-UserDetails.hasOne(Cart, { onDelete: 'CASCADE' })
-Cart.belongsTo(UserDetails, { onDelete: 'CASCADE' })
+// UserDetails.hasOne(Cart, { onDelete: 'CASCADE' })
+// Cart.belongsTo(UserDetails, { onDelete: 'CASCADE' })
 
-/////////////////////////////////////////////////////////////
+// /////////////////////////////////////////////////////////////
 
-Addresses.hasOne(OrderSummary, { onDelete: 'CASCADE' })
-OrderSummary.belongsTo(Addresses, { onDelete: 'CASCADE' })
+// Addresses.hasOne(OrderSummary, { onDelete: 'CASCADE' })
+// OrderSummary.belongsTo(Addresses, { onDelete: 'CASCADE' })
 
-/////////////////////////////////////////////////////////////
+// /////////////////////////////////////////////////////////////
 
-Product.hasOne(Reviews, { onDelete: 'CASCADE' })
-Reviews.belongsTo(Product, { onDelete: 'CASCADE' })
+// Product.hasOne(Reviews, { onDelete: 'CASCADE' })
+// Reviews.belongsTo(Product, { onDelete: 'CASCADE' })
 
-/////////////////////////////////////////////////////////////
+// /////////////////////////////////////////////////////////////
 
-Categorys.hasMany(Product, { onDelete: 'CASCADE' })
-Product.belongsTo(Categorys, {
-  //PREGUNTAR LA RELACION!! DUDA
-})
+// Categorys.hasMany(Product, { onDelete: 'CASCADE' })
+// Product.belongsTo(Categorys, {
+//   //PREGUNTAR LA RELACION!! DUDA
+// })
 
-/////////////////////////////////////////////////////////////
+// /////////////////////////////////////////////////////////////
 
-Cart.hasOne(OrderSummary, { onDelete: 'CASCADE' })
+// Cart.hasOne(OrderSummary, { onDelete: 'CASCADE' })
 
-OrderSummary.belongsTo(Cart, {
-  onDelete: 'CASCADE',
-})
+// OrderSummary.belongsTo(Cart, {
+//   onDelete: 'CASCADE',
+// })
 
-Cart.belongsToMany(Product, { through: 'Cart_has_Product' })
-Product.belongsToMany(Cart, { through: 'Cart_has_Product' })
+// Cart.belongsToMany(Product, { through: 'Cart_has_Product' })
+// Product.belongsToMany(Cart, { through: 'Cart_has_Product' })
