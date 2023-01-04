022c51387d7cb629cc1ef3c94325f1d821700f1f
JC <juancruzmusso@gmail.com>
2023-01-05 00:14:23 +0100

    ultima

--- backend/src/loaders/sequelize/index.js
+++ backend/src/loaders/sequelize/index.js
@@ -14,17 +14,22 @@ const sequelize =
   // );
   config.nodeEnv === "production"
     ? new Sequelize(
-        config.dataBase.name,
-        config.dataBase.username,
-        config.dataBase.password,
+        `postgresql://postgres:fpc8KFKHRh5HyAjV9YbT@containers-us-west-21.railway.app:6699/railway`,
         {
-          host: config.dataBase.host,
-          dialect: config.dataBase.dialect,
-          logging: false,
+          logging: false, // set to console.log to see the raw SQL queries
+          native: false, // lets Sequelize know we can use pg-native for ~30% more speed
         }
+        // config.dataBase.name,
+        // config.dataBase.username,
+        // config.dataBase.password,
+        // {
+        //   host: config.dataBase.host,
+        //   dialect: config.dataBase.dialect,
+        //   logging: false,
+        // }
       )
     : new Sequelize(
-        `postgresql://${config.dataBase.username}:${config.dataBase.password}@${config.dataBase.host}:${config.dataBase.port}/${config.dataBase.host}`,
+        `postgresql://${config.dataBase.username}:${config.dataBase.password}@${config.dataBase.host}:${config.dataBase.port}/${config.dataBase.name}`,
         {
           logging: false, // set to console.log to see the raw SQL queries
           native: false, // lets Sequelize know we can use pg-native for ~30% more speed
